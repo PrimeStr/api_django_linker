63342ca5f250212e39d7d14d00d51204e910dc51
Maxim Golovin <boss@primestr.ru>
2023-06-04 21:17:07 +0500

    Fix according to review.

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG', default='True')
+DEBUG = os.getenv('DEBUG', default=True)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='[]')
 
